7fc7b1a61f30fe3495abea64220d60c2c2e97fce
Saleh Azgor Rishad <shalahazgorrishad.40@gmail.com>
2026-09-06 04:37:31 +0600

    Delete SmartStudentApp directory

--- SmartStudentApp/src/service/ReportGenerator.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-// Interface for generating reports (OOP concept - Interface / Abstraction)
-// Any class that generates a report must implement this
-public interface ReportGenerator {
-    String generateReport();
-}
